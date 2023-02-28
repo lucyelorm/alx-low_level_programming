9bdab7d51dae64c8bcd6eabcd40620012f2109da
Lucy Ahadzi <lucyahadzi@gmail.com>
2023-02-28 11:48:26 -0800

    Task 03

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints a string
  * @str: The string
  */
+
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);/*counts each allocated character memory*/
-
-	_putchar('\n');
+	for (; *str != '\0'; str++)
+	{
+	putchar(*str);
+	}
+	putchar('\n');
 }
